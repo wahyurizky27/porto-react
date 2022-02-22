932769f6ecc2a6bbbe0253af563257b187690d27
wahyu rizky ramadhan <wahyurizkyramadhan007@gmail.com>
2022-02-22 19:22:39 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+    {
+      title: "React"
+    },
+    {
+      title: "Vue"
+    },
+    {
+      title: "Javascript"
+    },
+    {
+      title: "Next"
+    },
+    {
+      title: "Other"
+    },
+    {
+      title: "Git"
+    },
+  ];
+
+  export const testimonials = [
+    {
+        quote: "quote1",
+        image: "./project-2.gif",
+        name: "Wahyu RR",
+        company: "T05 techonologies"
+    },
+    {
+        quote: "quote2",
+        image: "./project-2.gif",
+        name: "Roxy",
+        company: "T05 techonologies"
+    },
+    {
+        quote: "quote3",
+        image: "./project-2.gif",
+        name: "BalqisR",
+        company: "T05 techonologies"
+    },
+    {
+        quote: "quote3",
+        image: "./project-2.gif",
+        name: "BalqisR",
+        company: "T05 techonologies"
+    },
+  ];
